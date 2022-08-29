e386e8e082d1a4bbf4dee7af2aa213f27f43deb2
Rui Lopes <rgl@ruilopes.com>
2022-08-29 09:01:45 +0100

    upgrade to runc 1.1.4

--- provision-runc.sh
+++ provision-runc.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # download.
 # see https://github.com/opencontainers/runc/releases
-runc_version='1.1.3'
+runc_version='1.1.4'
 runc_url="https://github.com/opencontainers/runc/releases/download/v${runc_version}/runc.amd64"
 bin="/tmp/runc-${runc_version}"
 wget -qO $bin "$runc_url"
