36e688687c6722933b216094ab13cbb643c49b86
Rui Lopes <rgl@ruilopes.com>
2023-01-21 16:59:54 +0000

    upgrade to regctl 0.4.5

--- provision-regctl.ps1
+++ provision-regctl.ps1
@@ -2,7 +2,7 @@
 
 # download install the binaries.
 # renovate: datasource=github-releases depName=regclient/regclient
-$version = '0.4.4'
+$version = '0.4.5'
 if (Test-Path "$env:ProgramFiles\regctl") {
     Remove-Item -Recurse -Force "$env:ProgramFiles\regctl"
 }
--- provision-regctl.sh
+++ provision-regctl.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # see https://github.com/regclient/regclient/releases
 # renovate: datasource=github-releases depName=regclient/regclient
-version='0.4.4'
+version='0.4.5'
 
 for tool in regctl regbot regsync; do
     # download.
