93a2c53a2050b476304e900b9ec1c6eec6913994
Rui Lopes <rgl@ruilopes.com>
2023-05-12 08:55:32 +0100

    upgrade to cni plugins 1.3.0

--- provision-cni-plugins.sh
+++ provision-cni-plugins.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # download.
 # see https://github.com/containernetworking/plugins/releases
 # renovate: datasource=github-releases depName=containernetworking/plugins
-cni_plugins_version='1.2.0'
+cni_plugins_version='1.3.0'
 cni_plugins_url="https://github.com/containernetworking/plugins/releases/download/v${cni_plugins_version}/cni-plugins-linux-amd64-v${cni_plugins_version}.tgz"
 tgz="/tmp/cni_plugins-${cni_plugins_version}.tgz"
 wget -qO $tgz "$cni_plugins_url"
