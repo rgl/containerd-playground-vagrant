d07c1c0e7a3349800eab7c91906dba6b284efa79
Rui Lopes <rgl@ruilopes.com>
2024-01-18 06:49:15 +0000

    upgrade to crane 0.18.0

--- provision-crane.ps1
+++ provision-crane.ps1
@@ -2,7 +2,7 @@
 
 # download install the binaries.
 # renovate: datasource=github-releases depName=google/go-containerregistry
-$archiveVersion = '0.17.0'
+$archiveVersion = '0.18.0'
 $archiveUrl = "https://github.com/google/go-containerregistry/releases/download/v$archiveVersion/go-containerregistry_Windows_x86_64.tar.gz"
 $archiveName = Split-Path -Leaf $archiveUrl
 $archivePath = "$env:TEMP\$archiveName"
