3182242834e1ecc31c0e56613bf1a6a7f4f256ed
Rui Lopes <rgl@ruilopes.com>
2025-01-01 07:08:08 +0000

    remove the unused Get-WindowsContainers cmdlet

--- ps.ps1
+++ ps.ps1
@@ -25,47 +25,6 @@ function Write-Title($title) {
     Write-Output "#`n# $title`n#"
 }
 
-# see https://github.com/microsoft/Windows-Containers
-# see https://techcommunity.microsoft.com/t5/containers/announcing-a-new-windows-server-container-image-preview/ba-p/2304897
-# see https://blogs.technet.microsoft.com/virtualization/2018/10/01/incoming-tag-changes-for-containers-in-windows-server-2019/
-# see https://hub.docker.com/_/microsoft-windows-nanoserver
-# see https://hub.docker.com/_/microsoft-windows-servercore
-# see https://hub.docker.com/_/microsoft-windows-server
-# see https://hub.docker.com/_/microsoft-windows
-# see https://mcr.microsoft.com/v2/windows/nanoserver/tags/list
-# see https://mcr.microsoft.com/v2/windows/servercore/tags/list
-# see https://mcr.microsoft.com/v2/windows/server/tags/list
-# see https://mcr.microsoft.com/v2/windows/tags/list
-# see https://mcr.microsoft.com/v2/powershell/tags/list
-# see https://mcr.microsoft.com/v2/dotnet/sdk/tags/list
-# see https://mcr.microsoft.com/v2/dotnet/runtime/tags/list
-# see https://hub.docker.com/_/golang/
-# see https://docs.microsoft.com/en-us/windows/release-information/
-# see https://docs.microsoft.com/en-us/windows/release-health/windows-server-release-info
-# see Get-WindowsVersion at https://github.com/rgl/windows-vagrant/blob/master/example/summary.ps1
-function Get-WindowsContainers {
-    $currentVersionKey = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
-    $windowsBuildNumber = $currentVersionKey.CurrentBuildNumber
-    $windowsVersionTag = @{
-        '20348' = 'ltsc2022'    # Windows Server 2022 (21H2).
-        '17763' = '1809'        # Windows Server 2019 (1809).
-    }[$windowsBuildNumber]
-    @{
-        tag = $windowsVersionTag
-        nanoserver = "mcr.microsoft.com/windows/nanoserver`:$windowsVersionTag"
-        servercore = "mcr.microsoft.com/windows/servercore`:$windowsVersionTag"
-        server = if ($windowsBuildNumber -ge 20348) {
-            "mcr.microsoft.com/windows/server`:$windowsVersionTag"
-        } else {
-            "mcr.microsoft.com/windows`:$windowsVersionTag"
-        }
-        powershellNanoserver = "mcr.microsoft.com/powershell:7.2-nanoserver-$windowsVersionTag"
-        golangNanoserver = "golang:1.19-nanoserver-$windowsVersionTag"
-        dotnetSdkNanoserver = "mcr.microsoft.com/dotnet/sdk:6.0-nanoserver-$windowsVersionTag"
-        dotnetRuntimeNanoserver = "mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-$windowsVersionTag"
-    }
-}
-
 # wrap the choco command (to make sure this script aborts when it fails).
 function Start-Choco([string[]]$Arguments, [int[]]$SuccessExitCodes=@(0)) {
     $command, $commandArguments = $Arguments
