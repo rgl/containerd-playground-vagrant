31b98c71957d13cc9f6ad4d47b54e08759fd47de
Rui Lopes <rgl@ruilopes.com>
2025-02-25 08:35:45 +0000

    upgrade to cri-tools 1.32.0

--- provision-cri-tools.sh
+++ provision-cri-tools.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # download.
 # see https://github.com/kubernetes-sigs/cri-tools/releases
 # renovate: datasource=github-releases depName=kubernetes-sigs/cri-tools
-version='1.31.1'
+version='1.32.0'
 url="https://github.com/kubernetes-sigs/cri-tools/releases/download/v${version}/crictl-v${version}-linux-amd64.tar.gz"
 tgz="/tmp/cri-tools-${version}.tgz"
 wget -qO $tgz "$url"
