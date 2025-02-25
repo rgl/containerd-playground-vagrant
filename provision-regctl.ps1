e8b19ed8d588a99a5818351af63760ac5d04019e
Rui Lopes <rgl@ruilopes.com>
2025-02-25 08:35:28 +0000

    upgrade to regctl 0.8.2

--- provision-regctl.ps1
+++ provision-regctl.ps1
@@ -2,7 +2,7 @@
 
 # download install the binaries.
 # renovate: datasource=github-releases depName=regclient/regclient
-$version = '0.7.2'
+$version = '0.8.2'
 if (Test-Path "$env:ProgramFiles\regctl") {
     Remove-Item -Recurse -Force "$env:ProgramFiles\regctl"
 }
