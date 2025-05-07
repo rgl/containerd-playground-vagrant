c8e4ccb7dc55afcdc8b0f49af7a1c4f1fd501351
Rui Lopes <rgl@ruilopes.com>
2025-05-07 19:08:08 +0000

    upgrade to regctl 0.8.3

--- provision-regctl.ps1
+++ provision-regctl.ps1
@@ -2,7 +2,7 @@
 
 # download install the binaries.
 # renovate: datasource=github-releases depName=regclient/regclient
-$version = '0.8.2'
+$version = '0.8.3'
 if (Test-Path "$env:ProgramFiles\regctl") {
     Remove-Item -Recurse -Force "$env:ProgramFiles\regctl"
 }
--- provision-regctl.sh
+++ provision-regctl.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # see https://github.com/regclient/regclient/releases
 # renovate: datasource=github-releases depName=regclient/regclient
-version='0.8.2'
+version='0.8.3'
 
 for tool in regctl regbot regsync; do
     # download.
