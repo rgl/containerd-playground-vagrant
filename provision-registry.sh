1a4f91e81abc41f57db0360ff6de8c009c587189
Rui Lopes <rgl@ruilopes.com>
2025-06-03 06:59:35 +0100

    upgrade to distribution registry 3.0.0

--- provision-registry.sh
+++ provision-registry.sh
@@ -5,7 +5,7 @@ registry_domain="${1:-registry.test}"; shift || true
 registry_host="$registry_domain:5000"
 registry_url="https://$registry_host"
 # renovate: datasource=docker depName=registry
-registry_image_version='2.8.3'
+registry_image_version='3.0.0'
 registry_image="registry:$registry_image_version" # see https://hub.docker.com/_/registry
 registry_username='vagrant'
 registry_password='vagrant'
@@ -36,11 +36,13 @@ echo -n 'http secret' >/opt/registry/secrets/http
 # create the configuration file.
 # NB this configures the registry to allow any url in the pushed manifests.
 #    see https://github.com/rgl/infra-toolbox/commit/28c2703f437b4ae4feb103948fea68ff8b02ca45
-# see https://github.com/distribution/distribution/blob/v2.8.3/docs/configuration.md
+# see https://distribution.github.io/distribution/about/configuration/
 install -d /opt/registry/etc
 cat >/opt/registry/etc/config.yml <<EOF
 version: 0.1
 log:
+  level: info
+  formatter: text
   fields:
     service: registry
 storage:
@@ -67,7 +69,8 @@ validation:
 EOF
 
 # launch the registry.
-# see https://docs.docker.com/registry/deploying/
+# see https://distribution.github.io/distribution/about/deploying/
+# see https://opentelemetry.io/docs/specs/otel/configuration/sdk-environment-variables/
 echo "starting the registry $registry_url..."
 install -d -m 700 /opt/registry/data
 nerdctl pull --quiet "$registry_image"
@@ -75,7 +78,7 @@ nerdctl run -d \
     --restart unless-stopped \
     --name registry \
     -p 5000:5000 \
-    -v /opt/registry/etc:/etc/docker/registry:ro \
+    -v /opt/registry/etc:/etc/distribution:ro \
     -v /opt/registry/data:/var/lib/registry \
     -v /opt/registry/secrets:/run/secrets \
     -e REGISTRY_HTTP_SECRET=/run/secrets/http \
@@ -85,6 +88,10 @@ nerdctl run -d \
     -e REGISTRY_AUTH=htpasswd \
     -e 'REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm' \
     -e REGISTRY_AUTH_HTPASSWD_PATH=/run/secrets/htpasswd \
+    -e OTEL_SDK_DISABLED=true \
+    -e OTEL_TRACES_EXPORTER=none \
+    -e OTEL_METRICS_EXPORTER=none \
+    -e OTEL_LOGS_EXPORTER=none \
     "$registry_image"
 
 # wait for the registry to be available.
@@ -104,4 +111,4 @@ nerdctl exec "$container_name" registry --version
 echo "registry environment variables:"
 nerdctl exec "$container_name" env
 echo "registry config:"
-nerdctl exec "$container_name" cat /etc/docker/registry/config.yml
+nerdctl exec "$container_name" cat /etc/distribution/config.yml
